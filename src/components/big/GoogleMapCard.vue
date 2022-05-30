5d23abed35f19a1cb07736c1e5775daaeddd586b
gros-eliot <eliot.gros@edu.univ-fcomte.fr>
2022-05-30 16:40:59 +0200

    googlemapcard : terminée (lien vers lieu google map)

--- src/components/big/GoogleMapCard.vue
+++ src/components/big/GoogleMapCard.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="ml-auto mr-auto flex h-[20rem] w-11/12 max-w-2xl rounded-3xl bg-[url('src/assets/pictos/map.jpg')] bg-cover bg-center">
+  <div class="ml-auto mr-auto flex h-[20rem] w-11/12 max-w-2xl rounded-xl bg-[url('src/assets/pictos/map.jpg')] bg-cover bg-center">
     <div class="flex w-full items-end">
-      <div class="flex w-full flex-row items-center justify-between gap-1 rounded-b-3xl bg-gray-900 p-5 text-yellow-400">
+      <div class="flex w-full flex-row items-center justify-between gap-1 rounded-b-xl bg-gray-900 p-5 text-yellow-400">
         <h3 class="text-xl font-bold md:text-3xl">{{ mapcardTitle }}</h3>
         <a
           href="https://www.google.com/maps/place/Mairie+de+Belfort/@47.6379236,6.8606643,17z/data=!3m1!4b1!4m5!3m4!1s0x47923b5f4ba75a69:0x1c99bbb93747b3ac!8m2!3d47.63792!4d6.862853"
